a2eeb2b5ad5e9b9adf16a500c0a9682b4d45ccd2
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-29 14:39:37 -0600

    form hashed out

--- screens/FormScreens/BudgetFormScreen.tsx
+++ screens/FormScreens/BudgetFormScreen.tsx
@@ -1,14 +1,54 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import CenterView from '../../components/myComponents/CenterView'
 
 
 const BudgetFormScreen = (props :any) => {
     return (
-        <CenterView>
+        <ScrollView >
+            <View style={styles.screen}>
             <Text>Form screen</Text>
-        </CenterView>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            <View style={styles.input}>
+                <TextInput />
+            </View>
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    input: {
+        width: '80%',
+        marginVertical: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+    },
+    form: {
+
+    },
+
+})
 
 export default BudgetFormScreen
