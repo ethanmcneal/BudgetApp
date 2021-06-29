7843ac75cd6c5de1b207542fdd8fc1473525b5e5
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-29 15:30:42 -0600

    figuring out good styling and flow for the form

--- screens/FormScreens/BudgetFormScreen.tsx
+++ screens/FormScreens/BudgetFormScreen.tsx
@@ -1,55 +1,77 @@
-import React from 'react'
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
-import CenterView from '../../components/myComponents/CenterView'
+import React, { useState } from "react";
+import {
+	Button,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from "react-native";
+import CenterView from "../../components/myComponents/CenterView";
 
+const BudgetFormScreen = (props: any) => {
+	const [income, setIncome] = useState('');
+	const [expenses, setExpenses] = useState([]);
 
-const BudgetFormScreen = (props :any) => {
-    return (
-        <ScrollView >
-            <View style={styles.screen}>
-            <Text>Form screen</Text>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <View style={styles.input}>
-                <TextInput />
-            </View>
-            <Button onPress={() => {props.navigation.navigate('Results')}} title='Submit'/>
-            </View>
-        </ScrollView>
-    )
-}
+	return (
+		<ScrollView>
+			<View style={styles.screen}>
+				<Text>Form screen</Text>
+				<View style={styles.inputContainer}>
+                    <Text style={styles.inputPrompt}>How many Sources of Income do you have?</Text>
+                    <View style={styles.input}>
+					<TextInput
+                    placeholder=''
+                    value={income} 
+                    />
+                    </View>
+				</View>
+				<View style={styles.inputContainer}>
+				</View>
+				<View style={styles.input}>
+					<TextInput />
+				</View>
+				<View style={styles.input}>
+					<TextInput />
+				</View>
+				<View style={styles.input}>
+					<TextInput />
+				</View>
+				<View style={styles.input}>
+					<TextInput />
+				</View>
+				<View style={styles.input}>
+					<TextInput />
+				</View>
+				<Button
+					onPress={() => {
+						props.navigation.navigate("Results");
+					}}
+					title="Submit"
+				/>
+			</View>
+		</ScrollView>
+	);
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
+	screen: {
+		flex: 1,
+		alignItems: "center",
+	},
+    inputContainer: {
         alignItems: 'center',
     },
-    input: {
-        width: '80%',
-        marginVertical: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
+    inputPrompt: {
+        width: '70%'
     },
-    form: {
+	input: {
+		width: "60%",
+		marginVertical: 20,
+		borderBottomWidth: 1,
+		borderBottomColor: "#ccc",
+	},
+	form: {},
+});
 
-    },
-
-})
-
-export default BudgetFormScreen
+export default BudgetFormScreen;
