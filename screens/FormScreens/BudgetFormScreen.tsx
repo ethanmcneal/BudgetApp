efeb18f8dc44514efe4b27dc7de18db1033c6a39
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-30 08:11:34 -0600

    form styling

--- screens/FormScreens/BudgetFormScreen.tsx
+++ screens/FormScreens/BudgetFormScreen.tsx
@@ -8,22 +8,29 @@ import {
 	TextInput,
 	View,
 } from "react-native";
+import Colors from "../../constants/Colors";
 
 const BudgetFormScreen = (props: any) => {
 	const [income, setIncome] = useState('');
-	const [expenses, setExpenses] = useState({house: '', utilities: '', car: '', food: '', leisure: '', otherPayments: '', });
+	const [expenses, setExpenses] = useState({house: '', utilities: '', car: '', food: '', leisure: '', other: '', });
 	const [counter, setCounter] = useState(0)
+
+	const handleChange = (e :any) => {
+		console.log(e.target.text)
+	}
 	return (
 		<ScrollView>
 			<View style={styles.screen}>
 				<Text>{counter}</Text>
 	{counter === 0 && <View style={styles.inputContainer}>
-					<Ionicons iconName={'ios-money'} size={22}/>
-                    <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
+                    <Text style={styles.inputPrompt}>How much Income do you make in a month?</Text>
+					<Ionicons name='cash' size={42} color='green'/>
                     <View style={styles.input}>
 					<TextInput
+					style={styles.field}
                     placeholder=''
-                    value={income} 
+                    value={income}
+					onChangeText={(text) => setIncome(text)} 
                     />
                     </View>
 				</View>}
@@ -32,8 +39,10 @@ const BudgetFormScreen = (props: any) => {
                     <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
                     <View style={styles.input}>
 					<TextInput
+					style={styles.field}
                     placeholder=''
-                    value={income} 
+                    value={expenses.house}
+					onChange={(e) => handleChange(e)} 
                     />
                     </View>
 				</View>}
@@ -42,8 +51,9 @@ const BudgetFormScreen = (props: any) => {
                     <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
                     <View style={styles.input}>
 					<TextInput
+					style={styles.field}
                     placeholder=''
-                    value={income} 
+                    value={expenses.utilities} 
                     />
                     </View>
 				</View>}
@@ -52,8 +62,9 @@ const BudgetFormScreen = (props: any) => {
                     <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
                     <View style={styles.input}>
 					<TextInput
+					style={styles.field}
                     placeholder=''
-                    value={income} 
+                    value={expenses.car} 
                     />
                     </View>
 				</View>}
@@ -62,11 +73,37 @@ const BudgetFormScreen = (props: any) => {
                     <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
                     <View style={styles.input}>
 					<TextInput
+					style={styles.field}
                     placeholder=''
-                    value={income} 
+                    value={expenses.food} 
+                    />
+                    </View>
+					
+				</View>}
+	{counter === 4 && <View style={styles.inputContainer}>
+					<Ionicons iconName={'ios-money'} size={22}/>
+                    <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
+                    <View style={styles.input}>
+					<TextInput
+					style={styles.field}
+                    placeholder=''
+                    value={expenses.leisure} 
+                    />
+                    </View>
+					
+				</View>}
+	{counter === 4 && <View style={styles.inputContainer}>
+					<Ionicons iconName={'ios-money'} size={22}/>
+                    <Text style={styles.inputPrompt}>How much Income do you make in any given month?</Text>
+                    <View style={styles.input}>
+					<TextInput
+					style={styles.field}
+                    placeholder=''
+                    value={expenses.other} 
                     />
                     </View>
 				</View>}
+				
 				<View style={styles.buttonContainer}>
 				{counter > 0 && <Button
 					onPress={() => {
@@ -93,16 +130,23 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
     inputContainer: {
+		marginVertical: 30,
         alignItems: 'center',
     },
     inputPrompt: {
-		fontSize: 22
+		fontSize: 22,
+		textAlign: 'center'
     },
 	input: {
-		width: "60%",
-		marginVertical: 20,
-		borderWidth: 2,
-		borderColor: "#ccc",
+		width: 300,
+		fontSize: 32,
+		borderBottomWidth: 2,
+		borderBottomColor: "#ccc",
+	},
+	field: {
+		fontSize: 33,
+		color: Colors.light.text,
+		textAlign: 'center',
 	},
 	form: {},
 	buttonContainer: {
